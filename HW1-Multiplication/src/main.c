8b93dd73a5e3583ab3f0137b120302bc482313cf
Dan <grezindanil@gmail.com>
2025-09-28 23:47:41 +0300

    add multiplication algorithm

--- HW1-Multiplication/src/main.c
+++ HW1-Multiplication/src/main.c
@@ -4,11 +4,8 @@ int multiplication(int x) {
     int xPow = x * x;
     return (xPow + 1) * (xPow + x) + 1;
 }
-<<<<<<< HEAD
-=======
 
 int main() {
     printf("%d", multiplication(2));
     return 0;
 }
->>>>>>> d954746 (add multiplication algorithm)
