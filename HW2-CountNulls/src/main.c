3742d75e906e0b0a9f1087a2232fd3d670ccfa94
Dan <grezindanil@gmail.com>
2025-09-30 20:30:20 +0300

    Format main.c

--- HW2-CountNulls/src/main.c
+++ HW2-CountNulls/src/main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-int countNulls(int* arr, int arrSize){
+int countNulls(int *arr, int arrSize) {
     int count = 0;
-    for(int i = 0; i < arrSize; i++){
-        if(arr[i] == 0)
+    for (int i = 0; i < arrSize; i++) {
+        if (arr[i] == 0)
             count++;
     }
     return count;
 }
 
-int main(){
+int main() {
     int arr[] = {1, 2, 3, 0, 3, 6, 0, 0, 0, -4, 0};
     int nullsCount = countNulls(arr, sizeof(arr) / sizeof(arr[0]));
     printf("%d", nullsCount);
     return 0;
 }
+
